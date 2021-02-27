0576c4451aabd433a62f417a86ca0e0bf8cdbea6
mehmet.yasin.yanik <myasin.yanik@gmail.com>
2021-02-27 19:08:05 +0300

    Add Core class

---
+++ AuthServer.Core/Model/UserApp.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthServer.Core.Model
+{
+    public class UserApp: IdentityUser
+    {
+        public string City { get; set; }
+    }
+}
