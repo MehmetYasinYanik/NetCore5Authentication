68fbcd2b4d016d13f50aaa114679251278dcbcc7
mehmet.yasin.yanik <myasin.yanik@gmail.com>
2021-02-28 13:43:05 +0300

    Add ITokenService

---
+++ AuthServer.Core/Configuration/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthServer.Core.Configuration
+{
+    public class Client
+    {
+        public string Id { get; set; }
+        public string Secret { get; set; }
+
+        // Hangi API lara erişim sağlanacak
+        // www.myapi1.com www.myapi.2.com
+        public List<string> Audiences { get; set; } 
+    }
+}
---
+++ AuthServer.Core/Services/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using AuthServer.Core.Configuration;
+using AuthServer.Core.Dto;
+using AuthServer.Core.Model;
+
+namespace AuthServer.Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userApp);
+        ClientTokenDto CreateTokenyClient(Client client);
+    }
+}
