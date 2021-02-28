8f7fbaa6563c32f1987db316ae48a7ea83337df6
mehmet.yasin.yanik <myasin.yanik@gmail.com>
2021-02-28 14:58:56 +0300

    Add IUserSerivece

---
+++ AuthServer.Core/Dto/UserDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace AuthServer.Core.Dto
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ AuthServer.Core/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using AuthServer.Core.Dto;
+using Shared.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IUserService
+    {
+        Task<Response<UserDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<Response<UserDto>> GetUserByNameAsync(string username);
+    }
+}
