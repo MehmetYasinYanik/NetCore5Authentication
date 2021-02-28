73484639a6f7c9c9b87214ac0fb20ab4c3aa7cc3
mehmet.yasin.yanik <myasin.yanik@gmail.com>
2021-02-28 16:32:54 +0300

    Add DbContext

---
+++ AuthServer.Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using AuthServer.Core.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServer.Data
+{
+    public class AppDbContext : IdentityDbContext<UserApp,IdentityRole,string>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<UserRefreshToken> UserRefreshToken { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
