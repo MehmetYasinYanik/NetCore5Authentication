804bbc5c84794878263673ae17160044cf5d8aa5
mehmet.yasin.yanik <myasin.yanik@gmail.com>
2021-02-28 13:27:28 +0300

    Add IService

---
+++ AuthServer.Core/Services/IService.cs
@@ -0,0 +1,18 @@
+﻿using Shared.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IService<TEntity, TDto> where TEntity : class where TDto : class
+    {
+        Task<Response<TDto>> GetByIdAsync(int id);
+        Task<Response<IEnumerable<TDto>>> GetAllAsync();
+        Task<Response<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> predicate);
+        Task<Response<TDto>> AddAsync(TEntity entity);
+        Task<Response<EmptyDataDto>> Remove(TEntity entity);
+        Task<Response<EmptyDataDto>> Update(TEntity entity);
+    }
+}
