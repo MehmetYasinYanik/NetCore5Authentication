fd9b43eec6e4e4b167fcba1d9d75858d0a7a9301
mehmet.yasin.yanik <myasin.yanik@gmail.com>
2021-02-28 11:24:02 +0300

    Add errorDto

---
+++ Shared/Dto/ErrorDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Dto
+{
+    public class ErrorDto
+    {
+        public List<String> Errors { get; set; }
+
+        public bool IsShow { get; set; }
+
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+
+        public ErrorDto(string error, bool isShow)
+        {
+            Errors.Add(error);
+            IsShow = isShow;
+        }
+        public ErrorDto(List<string> errors, bool isShow)
+        {
+            Errors = errors;
+            IsShow = isShow;
+        }
+    }
+}
