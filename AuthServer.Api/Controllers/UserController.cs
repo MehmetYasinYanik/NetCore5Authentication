542a67405f4626b462e7e49449a5b4c4fd53d198
mehmet.yasin.yanik <myasin.yanik@gmail.com>
2021-07-14 12:20:12 +0300

    #42 Add User Controller

--- AuthServer.Api/Controllers/UserController.cs
+++ AuthServer.Api/Controllers/UserController.cs
@@ -3,9 +3,6 @@ using AuthServer.Core.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace AuthServer.API.Controllers
