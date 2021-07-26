211cb2a2cf66b3f006b469d26d26be5c654170d4
mehmet.yasin.yanik <myasin.yanik@gmail.com>
2021-07-26 14:21:16 +0300

    #46

---
+++ ExtermalTest1.Api/Controllers/StockController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Security.Claims;
+
+namespace ExternalTest1.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        //46
+        public IActionResult GetStock() 
+        {
+            var username = HttpContext.User.Identity.Name;
+            var userId = User.Claims.FirstOrDefault(i => i.Type == ClaimTypes.NameIdentifier);
+            return Ok($"(Stock) Username : {username} - User ID : {userId}");
+        }
+    }
+}
---
+++ ExtermalTest2.Api/Controllers/InvoiceController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace ExternalTest1.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        //46
+        public IActionResult GetInvoice()
+        {
+            var username = HttpContext.User.Identity.Name;
+            var userId = User.Claims.FirstOrDefault(i => i.Type == ClaimTypes.NameIdentifier);
+            return Ok($"(Invoice) Username : {username} - User ID : {userId}");
+        }
+    }
+}
