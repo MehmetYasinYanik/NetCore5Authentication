832057d06e644ea76166fb0db35cd3eab3c2536b
mehmet.yasin.yanik <myasin.yanik@gmail.com>
2021-08-10 17:43:38 +0300

    #47

--- ExtermalTest1.Api/Controllers/StockController.cs
+++ ExtermalTest1.Api/Controllers/StockController.cs
@@ -12,11 +12,12 @@ namespace ExternalTest1.API.Controllers
     public class StockController : ControllerBase
     {
         //46
+        [HttpGet]
         public IActionResult GetStock() 
         {
             var username = HttpContext.User.Identity.Name;
-            var userId = User.Claims.FirstOrDefault(i => i.Type == ClaimTypes.NameIdentifier);
-            return Ok($"(Stock) Username : {username} - User ID : {userId}");
+            var userIdClaim = User.Claims.FirstOrDefault(i => i.Type == ClaimTypes.NameIdentifier);
+            return Ok($"(Stock) Username : {username} - User ID : {userIdClaim.Value}");
         }
     }
 }
--- ExtermalTest2.Api/Controllers/InvoiceController.cs
+++ ExtermalTest2.Api/Controllers/InvoiceController.cs
@@ -15,11 +15,12 @@ namespace ExternalTest1.API.Controllers
     public class InvoiceController : ControllerBase
     {
         //46
+        [HttpGet]
         public IActionResult GetInvoice()
         {
             var username = HttpContext.User.Identity.Name;
-            var userId = User.Claims.FirstOrDefault(i => i.Type == ClaimTypes.NameIdentifier);
-            return Ok($"(Invoice) Username : {username} - User ID : {userId}");
+            var userIdClaim = User.Claims.FirstOrDefault(i => i.Type == ClaimTypes.NameIdentifier);
+            return Ok($"(Invoice) Username : {username} - User ID : {userIdClaim.Value}");
         }
     }
 }
