7a7232578d833b6dbaebd17b3576d33301ecf23d
mehmet.yasin.yanik <myasin.yanik@gmail.com>
2021-08-11 19:16:01 +0300

    #49

---
+++ AuthServer.Api/Validations/CreateUserDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using AuthServer.Core.Dto;
+using FluentValidation;
+
+namespace AuthServer.API.Validations
+{
+    public class CreateUserDtoValidator : AbstractValidator<CreateUserDto>
+    {
+        public CreateUserDtoValidator()
+        {
+            RuleFor(i => i.Username).NotEmpty().WithMessage("Username is required");
+            RuleFor(i => i.Password).NotEmpty().WithMessage("Password is required");
+            RuleFor(i => i.Email).NotEmpty().WithMessage("Email is required").EmailAddress().WithMessage("Email is wrong");
+        }
+    }
+}
